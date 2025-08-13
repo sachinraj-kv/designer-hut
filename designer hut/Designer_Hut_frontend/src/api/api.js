0b15fe0dba232dc12b9797f85fcc15d7709b9c23
sachinraj kv <sachinrajkv22@gmail.com>
2025-08-13 17:25:40 +0530

    deploy-env

--- designer hut/Designer_Hut_frontend/src/api/api.js
+++ designer hut/Designer_Hut_frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-    baseURL : "http://localhost:3000",
-     withCredentials: true
-})
+    baseURL: API_BASE_URL,
+    withCredentials: true
+});
